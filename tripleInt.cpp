9daf55e24930c28fdfa2a07116bc26f9bfb8cd7d
Laoxiong559 <0481711@my.scccd.edu>
2025-01-14 18:11:40 -0800

    Add files via upload

---
+++ tripleInt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+    cout << "Triple of " << num << " is " << num * 3 << endl;
+    return 0;
+}
