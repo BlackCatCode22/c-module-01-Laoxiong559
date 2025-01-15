370ef84d6ee9abe60638d4d59df5ce796bfdf615
Laoxiong559 <0481711@my.scccd.edu>
2025-01-14 17:47:29 -0800

    doubleInt.cpp

---
+++ doubleInt.cpp/doubleInt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+    cout << "Double of " << num << " is " << num * 2 << endl;
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
